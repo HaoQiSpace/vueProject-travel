c1f43f25ba7906eb375782bc01b9e59fe98a7be4
HQ-Bug <44158476+HaoQiSpace@users.noreply.github.com>
2019-03-08 21:11:57 +0800

    main finish

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <!-- exclude="Detail"：使得进入Detail页面不会被缓存，从而需要新发取ajax数据请求 -->
+    <keep-alive exclude="Detail">
+      <router-view/>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style></style>
